3786123de3f7bdd06e0133a375160da496520b05
SoledadOsk <soledadinmb@gmail.com>
2024-06-15 16:32:55 -0300

    estilos y header modificado, haciendo menu hambuurguesa
    aun no hice el menu, emprolije algunos detalles, siguiente paso menu .

--- header.js
+++ header.js
@@ -21,30 +21,34 @@ let header=`<!-- Marquesina de propaganda -->
     </ul>
 </div>
 
-<!-- enlaces iconicos -->
+
 <div class="header-icon-nav">
+
     <div class="header-icon-links">
         <ul>
-            <li><a href="suscribite.html">Suscribite&nbsp;&nbsp;&nbsp;</a></li>
-            <li><a href="locales.html">Locales&nbsp;&nbsp;&nbsp; </a></li>
-            <li><a href="help.html">Ayuda&nbsp;&nbsp;&nbsp;</a></li>
+            <li><a href="suscribite.html">Suscribite</a></li>
+            <li><a href="locales.html">Locales</a></li>
+            <li><a href="help.html">Ayuda</a></li>
         </ul>
     </div>
     
-    <div class="nav-items">
-        <div class="nav-logo">
-            <a href="index.html"><img id="logo" src="imagenes/logosj.png" alt="Logo Indumentaria Informal"></a>
-        </div>
-        <ul class="nav-menu">
+    
+    <div class="nav-logo">
+         <a href="index.html"><img id="logo" src="imagenes/logosj.png" alt="Logo Indumentaria Informal"></a>
+    
+         </div>
+      
+    <div class="nav-menu">
+        <ul>
             <li class="menu-item"><a href="prod_hombres.html">HOMBRE</a></li>
             <li class="menu-item"><a href="prod_mujeres.html">MUJER</a></li>
             <li class="menu-item"><a href="prod_ninos.html">NIÑOS</a></li>
         </ul>
-        <div class="nav-buscar">
-            <a href="#"><i class="bi bi-search"></i></a>
-            
-        </div>
+   
     </div> 
-</div>`
+       
+
+</div>
+`
 
 document.querySelector(".header").innerHTML=header;
